5e76ca5e8bef69cde95021f1c10851be3f993b1e
David Nishikawa <triptype@gmail.com>
2017-10-18 14:39:16 -0700

    pass green test for post returning mongoosed item && write red test for get:id

--- test/2e2/amino_acids.test.js
+++ test/2e2/amino_acids.test.js
@@ -5,39 +5,44 @@ const {assert} = require('chai');
 const arg = {
     name: 'arginine',
     abbrs: {
-        abbr1: 'arg',
-        abbr3: 'r'
-    }
+        abbr1: 'r',
+        abbr3: 'arg'
+    },
+    polar: true,
+    group: 'amine',
+    canonical: true
 };
 
 describe('amino acid route', () => {
+
     describe('post', () => {
         it('returns the items saved with it\'s new id', () => {
             return request.post('/api/amino-acids')
                 .send(arg)
                 .then(res => {
                     const saved = JSON.parse(res.text);
-                    assert.equal(
-                        saved,
-                        {
-                            name: 'arginine',
-                            abbrs: {
-                                abbr1: 'arg',
-                                abbr3: 'r'
-                            },
-                            _id: saved._id
-                        }
-                    );
+                    assert.equal(saved.name, arg.name);
+                    assert.equal(saved.abbrs.abbr1, saved.abbrs.abbr1);
                 });
         });
     });
 
     describe('get' , () => {
-
     });
     
     describe('get:id', () => {
-
+        it('gets an item by id', () => {
+            return request.post('/api/amino-acids')
+                .send(arg)
+                .then(res => {
+                    const saved = JSON.parse(res.text);
+                    return request.get(`/api/amino-acids/${saved._id}`)
+                        .then(data => {
+                            const gotten = JSON.parse(data.text);
+                            assert.deepEqual(gotten, saved);
+                        });
+                });
+        });
     });
 
     describe('delete', () => {
