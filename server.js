d0883189ab163d0c30d41a1f2fd6f603acd26990
ckrountree <jmail0207@gmail.com>
2017-10-18 10:36:38 -0700

    move files out of routes folder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const http = require('http');
-const app = require('./routes/app');
+const app = require('./lib/app');
 const connect = require('./lib/connect');
 
 connect();
 
 const server = http.createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 server.listen(port, () => {
-    console.log('server running on', server.address().port); //eslint-disable-line
+    console.log('server running on', server.address().port); // eslint-disable-line
 });
