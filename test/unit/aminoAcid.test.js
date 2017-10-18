4450be730f666963d6d97cc763a95cb7247ec57a
David Nishikawa <triptype@gmail.com>
2017-10-18 10:31:39 -0700

    green test passing for creating valid amino acid

--- test/unit/aminoAcid.test.js
+++ test/unit/aminoAcid.test.js
@@ -5,14 +5,14 @@ describe('amino acid model', () => {
     it('creates a valid amino-acid object', () => {
         const asparagine = new AA({
             name: 'asparagine',
-            abbr1: 'as',
+            abbr1: 'a',
             abbr3: 'asn',
             polar: true,
             sideChainFuncGroups: 'amide',
             canonical: true
         });
         assert.ok(asparagine instanceof AA);
-        const {errors} = asparagine.validateSync();
-        assert.ok(!errors);
+        const invalid = asparagine.validateSync();
+        assert.ok(!invalid);
     });
 });
