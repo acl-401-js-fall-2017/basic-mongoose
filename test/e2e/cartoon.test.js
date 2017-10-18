5eb310928d5e1352de40e9cf48f9dc5bea2b6938
Shane Moyo <smoyo@pdx.edu>
2017-10-18 11:56:41 -0700

    set up dependency and added save test

---
+++ test/e2e/cartoon.test.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const mongoose = require('mongoose');
+const request = require('./request');
+
+describe('Cartoons API', ()=>{
+
+    beforeEach(()=> mongoose.connection.dropDatabase());
+
+    const rugRats = {
+        name: 'Rugrats',
+        releaseYear: 1991
+    };
+
+    it('Saves a cartoon with id', ()=>{
+
+        return request.post('/api/cartoons')
+            .send(rugRats)
+            .then(res => {
+                const cartoon = res.body;
+                assert.ok(cartoon._id);
+                assert.equal(cartoon.name, rugRats.name);
+            });
+
+    });
+
+
+
+});
