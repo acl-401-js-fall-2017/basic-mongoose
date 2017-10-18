7137018cf07690925598499fa8b7902fc426e891
mirrorapps <eli.speigel@gmail.com>
2017-10-18 12:41:21 -0700

    all tests a go

--- test/unit/altcoin.test.js
+++ test/unit/altcoin.test.js
@@ -6,9 +6,30 @@ describe.only('Altcoin Model', () => {
         const altcoin = new Altcoin({
             name: 'Ethereum',
             ticker: 'ETH',
-            priceUSD: 54200,
+            price: {
+                USD: 5400,
+                hr24: {
+                    percentChange: 4.5,
+                    volumeUSD: 2250300000	
+                }
+            },
             hash: 'Ethash'
         });
         assert.equal(altcoin.validateSync(), undefined);
+    });
+
+    it('required fields', () => {
+        const altcoin = new Altcoin({});
+        const { errors } = altcoin.validateSync();
+        assert.equal(errors['name'].kind, 'required');
+        assert.equal(errors['price.USD'].kind, 'required');
+    });
+
+    it('hash must be valid', () => {
+        const altcoin = new Altcoin({
+            hash: 'x11'
+        });
+        const { errors } = altcoin.validateSync();
+        assert.equal(errors['hash'].kind, 'enum');
     });
 });
