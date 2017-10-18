e49daee5b3882f8bb71a895a193dd9bf9e4ce802
Maryussuperboy <martsyalis@gmail.com>
2017-10-18 12:55:52 -0700

    tried to create a post that accepts more then one argument so far no good

---
+++ test/e2e/e2e-test.js
@@ -0,0 +1,67 @@
+const request = require('./request');
+const mongoose = require('mongoose');
+const { assert } = require('chai');
+
+
+describe ('cities API CRUD', () => {
+
+    beforeEach(() => mongoose.connection.dropDatabase());
+
+    const portland = {
+        name: 'Portland',
+        weather: 'dreadful',
+        population: 1000000,
+        attractions: [
+            { 
+                name: 'Rose&Thistle',                  
+                type: 'bar',
+                price: 'cheap'
+            },
+            {
+                name: 'OMSI after dark',
+                type: 'museum',
+                price: 'moderate',
+                date: 'every 3rd wednesday'
+            }
+        ]
+    }; 
+
+    const paris = {
+        name: 'Paris',
+        weather: 'snobby',
+        population: 5000000,
+    }; 
+
+
+
+    it('Post should save with id', () => {
+        return request.post('/api/cities')
+            .send(portland)
+            .then(res => {
+                assert.ok(res.body._id);
+                assert.equal(res.body.name, portland.name);
+            });
+    });
+
+    it('Gets all posted cities', () => {
+        return request.post('/api/cities')
+        //Question: why cant i send both cities like send(portland, paris)
+            .send(portland)
+            .then (() => {
+                return request.post('/api/cities')
+                    .send(paris);
+            })
+            .then (() => {
+                return request.get('/api/cities');
+            })
+            .then(res => {
+                assert.equal(res.body[1].name, paris.name);
+                assert.equal(res.body[0].name, portland.name);
+            });
+    });
+
+
+
+
+
+});
