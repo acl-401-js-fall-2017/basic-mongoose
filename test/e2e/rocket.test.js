0862a919026212eeb1090ffd6fc9b6c1c0f7f10f
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-19 00:04:25 -0700

    get by id passed

--- test/e2e/rocket.test.js
+++ test/e2e/rocket.test.js
@@ -6,4 +6,40 @@ describe('rockets rest API', () => {
 
     beforeEach(() => mongoose.connection.dropDatabase());
 
+    const apollo = {
+        name: 'apollo',
+        dateLaunched: new Date(),
+        launchNumber: 11
+    };
+
+    it('it saves a rocket by id', () => {
+        return request.post('/api/rockets')
+            .send(apollo)
+            .then(res => {
+                const dbRocket = res.body;
+                assert.ok(dbRocket._id);
+                assert.equal(dbRocket.name, apollo.name);
+            });
+    });
+
+    it('get saved object with id', () => {
+        let skylab = {
+            name: 'skylab',
+            dateLaunched: new Date(),
+            launchNumber: 1
+        };
+
+
+        let dbRocket = null;
+        return request.post('/api/rockets')
+            .send(skylab)
+            .then(res => {
+                dbRocket = res.body;
+                return request.get(`/api/rockets/${dbRocket._id}`);
+            });
+            // .then(res => {
+            //     assert.deepEqual(res.body, dbRocket);
+            // });
+    });
+
 });
