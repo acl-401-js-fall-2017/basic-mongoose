32e8953ca472f3d3a00dbeababf8275c8a32f535
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-19 00:17:19 -0700

    get all test good

--- test/e2e/rocket.test.js
+++ test/e2e/rocket.test.js
@@ -6,13 +6,14 @@ describe('rockets rest API', () => {
 
     beforeEach(() => mongoose.connection.dropDatabase());
 
-    const apollo = {
-        name: 'apollo',
-        dateLaunched: new Date(),
-        launchNumber: 11
-    };
-
     it('it saves a rocket by id', () => {
+
+        let apollo = {
+            name: 'apollo',
+            dateLaunched: new Date(),
+            launchNumber: 11
+        };
+
         return request.post('/api/rockets')
             .send(apollo)
             .then(res => {
@@ -22,7 +23,7 @@ describe('rockets rest API', () => {
             });
     });
 
-    it('get saved object with id', () => {
+    it('get saved rocket with id', () => {
         let skylab = {
             name: 'skylab',
             dateLaunched: new Date(),
@@ -37,9 +38,45 @@ describe('rockets rest API', () => {
                 dbRocket = res.body;
                 return request.get(`/api/rockets/${dbRocket._id}`);
             });
-            // .then(res => {
-            //     assert.deepEqual(res.body, dbRocket);
-            // });
     });
+
+    it('returns 404 for getting a bad id', () => {
+        return request.get('/api/rockets/59e401db548d1096dde508a6')
+            .then(
+                () => { throw new Error('found incorrect id'); },
+                err => {
+                    assert.equal(err.status, 404);
+                });
+    });
+
+    it('gets all rockets from database', () => {
+        let skylab = {
+            name: 'skylab',
+            dateLaunched: new Date(),
+            launchNumber: 1
+        };
+        let apollo = {
+            name: 'apollo',
+            dateLaunched: new Date(),
+            launchNumber: 11
+        };
+
+        const dbRockets = [skylab, apollo].map(rocket => {
+            return request.post('/api/rockets')
+                .send(rocket)
+                .then(res => res.body);
+        });
+
+        let saved = null;
+        return Promise.all(dbRockets)
+            .then(_saved => {
+                saved = _saved;
+                return request.get('/api/rockets');
+            })
+            .then(res => {
+                assert.deepEqual(res.body, saved);
+            });
+    });
+
 
 });
