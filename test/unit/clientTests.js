5975c1a7866fe718cce92380c32939b947caefde
Christina Mills <chrimills@gmail.com>
2017-10-24 10:19:45 -0700

    commented out code removed

--- test/unit/clientTests.js
+++ test/unit/clientTests.js
@@ -26,27 +26,27 @@ describe('ClientModel', () => {
         assert.equal(errors.age.kind, 'min');
     });
 
-    // it('tests that name is required', () => {
-    //     const client = new Client({
-    //         phone: 1234567890
-    //     });
-    //     const { errors } = client.validateSync();
-    //     assert.equal(errors.name.kind, 'required');
-    // });
+    it('tests that name is required', () => {
+        const client = new Client({
+            phone: 1234567890
+        });
+        const { errors } = client.validateSync();
+        assert.equal(errors.name.kind, 'required');
+    });
 
-    // it('checks that number is not valid', () => {
-    //     const client = new Client({
-    //         phone: 123456
-    //     });
-    //     const { errors } = client.validateSync();
-    //     assert.equal(errors.phone.kind, 'regexp');
-    // });
+    it('checks that number is not valid', () => {
+        const client = new Client({
+            phone: 123456
+        });
+        const { errors } = client.validateSync();
+        assert.equal(errors.phone.kind, 'regexp');
+    });
 
-    // it('checks that services are available', () => {
-    //     const client = new Client({
-    //         service: 'delivery'
-    //     });
-    //     const { errors } = client.validateSync();
-    //     assert.equal(errors.service.kind, 'enum');
-    // });
+    it('checks that services are available', () => {
+        const client = new Client({
+            service: 'delivery'
+        });
+        const { errors } = client.validateSync();
+        assert.equal(errors.service.kind, 'enum');
+    });
 });
